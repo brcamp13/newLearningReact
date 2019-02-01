a0f9ebc9cb81eef5169657f9366f22cec3871455
Brandon.Campbell <brcamp13@yahoo.com>
2019-02-01 09:50:46 -0800

    Did the development environment tutorial

---
+++ testapp/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HellowWorld from './HelloWorld';
+import "./index.css";
+
+ReactDOM.render(
+    <HellowWorld/>,
+    document.getElementById('root')
+);
